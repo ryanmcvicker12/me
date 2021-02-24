c05893db8c1b03d7e3c64c0bf4cffad4c3dd8ec6
ryanmcvicker12 <ryanmcvicker12@gmail.com>
2021-02-23 19:30:17 -0500

    changed cv to resume

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Ryan</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/1XkmHalTNZJ99QN2K1j4EKGjKNuul2YRoGtehHmgpjEU/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/1XkmHalTNZJ99QN2K1j4EKGjKNuul2YRoGtehHmgpjEU/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Resume<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
